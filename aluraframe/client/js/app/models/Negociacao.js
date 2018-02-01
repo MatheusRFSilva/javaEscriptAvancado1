8300959a0934e54e2092591a5a107f32b13a11e0
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-02-01 16:58:51 -0200

    Aula 02 - 06 Objeto imutaveis

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -3,9 +3,11 @@ class Negociacao {
       this._data = data;
       this._quantidade = quantidade ;
       this._valor = valor;
+      /*Congela o Objeto e não permite alteração*/
+      Object.freeze(this);
     }
 
-    /*Metodo (funcao dentro da classe)*/
+    /*Metodo*/
     get volume(){
       return this._quantidade * this._valor;
     }
