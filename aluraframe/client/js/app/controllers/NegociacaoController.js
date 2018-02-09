6fc760a04f9d403c62acd5dd6097d9857ec668ff
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-02-09 18:21:41 -0200

    Aula 03 - (1e2) Associando metodos do controller

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,15 @@
+class NegociacaoController {
+
+
+  adiciona(event){
+    event.preventDefault();
+    let $ = document.querySelector.bind(document);
+    let inputQuantidade = $('#quantidade');
+    let inputData = $('#data');
+    let inputValor = $('#valor');
+
+    console.log(inputData.value);
+    console.log(inputQuantidade.value);
+    console.log(inputValor.value);
+  }
+}
