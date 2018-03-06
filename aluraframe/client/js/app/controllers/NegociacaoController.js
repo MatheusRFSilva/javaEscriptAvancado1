953aefd8f09ccc8a9fec208e4cecd4e32609bb47
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-03-06 17:00:20 -0300

    Aula 04 - 01 Exibindo o objeto Date no formato dia/mês/ano

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -23,7 +23,10 @@ class NegociacaoController {
 
     );
 
-    console.log(negociacao);
+    let diaMesAno = negociacao.data.getDate()
+    +'/'+ (negociacao.data.getMonth() +1 )+'/'
+    +negociacao.data.getFullYear();
+    console.log(diaMesAno);
 
 
   }
