94777eaadcae418e219e686191d95fd6606c921a
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-03-07 10:41:43 -0300

    Aula 04-03 Métodos Estáticos

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -10,18 +10,18 @@ class NegociacaoController {
   adiciona(event){
     event.preventDefault();
 
-    let helper = new DateHelper();
-    let data = helper.textoParaData(this._inputData.value);
+    //let helper = new DateHelper();
+
 
     let negociacao =  new Negociacao(
-        data,
+        DateHelper.textoParaData(this._inputData.value),
         this._inputQuantidade.value,
         this._inputValor.value
 
     );
 
     console.log(negociacao);
-    console.log(helper.dataParaTexto(negociacao.data));
+    console.log(DateHelper.dataParaTexto(negociacao.data));
 
 
 
